8270699f09ecb7a2f36df2bbec72a73bf25d8f68
Robin Moretto <Zebul0n14@outlook.fr>
2024-06-13 17:42:43 +0200

    update(home) adding links to others pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Link href={"monster"}>Liste des Monstres</Link>
+      <br />
+      <Link href={"character"}>Liste des Personnages</Link>
+      <br />
+      <Link href={"map"}>Liste des Habitats</Link>
+      <br />
+      <Link href={"place"}>Liste des Lieux</Link>
+    </>
+  )
 }
