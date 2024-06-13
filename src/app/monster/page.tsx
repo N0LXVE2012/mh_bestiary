c39fcb5ee8693236997870ba34b90f7cf40e7c23
Robin Moretto <Zebul0n14@outlook.fr>
2024-06-13 15:00:04 +0200

    update(character): added monster data

--- src/app/monster/page.tsx
+++ src/app/monster/page.tsx
@@ -1,3 +1,36 @@
 export default function Monster() {
-  return <div>Monstre</div>;
+
+  type Monster = {
+    name: string;
+    map: string;
+  };
+
+  const monster_1: Monster = {
+    name: "Rathalos",
+    map: "Forêt ancienne"
+    
+  };
+
+  const monster_2: Monster = {
+    name: "Valstrax",
+    map: "Cime oubliée"
+  };
+
+  const monster_3: Monster = {
+    name: "Fatalis",
+    map: "Château Schrade"
+  };
+
+  return (
+    <>
+      <div>Monstre: {monster_1.name}</div>
+      <div>Habitat: {monster_1.map}</div>
+      <br />
+      <div>Monstre: {monster_2.name}</div>
+      <div>Habitat: {monster_2.map}</div>
+      <br />
+      <div>Monstre: {monster_3.name}</div>
+      <div>Habitat: {monster_3.map}</div>
+    </>
+  );
 }
