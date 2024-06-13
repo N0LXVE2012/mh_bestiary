c8233ca115dbe893aeb9b88c3ff8a4261a982985
Robin Moretto <Zebul0n14@outlook.fr>
2024-06-13 16:02:05 +0200

    refact(character): added objects array of characters

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -6,43 +6,34 @@ export default function Character() {
     village: string;
     dexterity?: number;
   };
+  const characters: Character[] = [
+    {
+      name: "Hinoa",
+      possesses: true,
+      weapons: ["Lance", "Arc"],
+      village: "Kamura",
+    },
+    {
+      name: "Minoto",
+      possesses: true,
+      weapons: ["Katana", "Corne de Chasse"],
+      village: "Kamura",
+    },
+    {
+      name: "Fugen",
+      weapons: ["Marteau", "Lames Doubles"],
+      village: "Kamura",
+    }
+  ]
 
-  const character_1: Character = {
-    name: "Hinoa",
-    possesses: true,
-    weapons: ["Lance", "Arc"],
-    village: "Kamura",
-  };
+  return characters.map((character: Character, index: number) => (
+    <div>
+      <p>Personnage: {character.name}</p>
+      <p>Possédé: {character.possesses ? "Oui" : "Non"}</p>
+      <p>Arme(s): {character.weapons.join(" / ")}</p>
+      <p>Village: {character.village}</p>
 
-  const character_2: Character = {
-    name: "Minoto",
-    possesses: true,
-    weapons: ["Katana", "Corne de Chasse"],
-    village: "Kamura",
-  };
-
-  const character_3: Character = {
-    name: "Fugen",
-    weapons: ["Marteau", "Lames Doubles"],
-    village: "Kamura",
-  };
-
-  return (
-    <>
-      <div>Personnage: {character_1.name}</div>
-      <div>Possédé: {character_1.possesses ? "Oui" : "Non"}</div>
-      <div>Arme(s): {character_1.weapons.join(" / ")}</div>
-      <div>Village: {character_1.village}</div>
       <br />
-      <div>Personnage: {character_2.name}</div>
-      <div>Possédé: {character_2.possesses ? "Oui" : "Non"}</div>
-      <div>Arme(s): {character_2.weapons.join(" / ")}</div>
-      <div>Village: {character_2.village}</div>
-      <br />
-      <div>Personnage: {character_3.name}</div>
-      <div>Possédé: {character_3.possesses ? "Oui" : "Non"}</div>
-      <div>Arme(s): {character_3.weapons.join(" / ")}</div>
-      <div>Village: {character_3.village}</div>
-    </>
-  );
+    </div>
+  ))
 }
